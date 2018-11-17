888377f04e63f5ba658fc575b8ffc89d22eb9515
Srikar Hanumanula <geetsrikar@gmail.com>
2018-11-17 14:08:14 -0500

    11/17/18 - Added ability to change direction and speed

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveBaseTesting.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveBaseTesting.java
@@ -19,15 +19,22 @@ public class driveBaseTesting extends TunableOpMode {
     robotBase robot                     = new robotBase();
     private ElapsedTime runtime         = new ElapsedTime();
 
+    int direction = 1;
+    int power = 0;
+
     double leftPower = 0.0;
     double rightPower = 0.0;
     double servoPower = 0.0;
 
+    boolean yDown = false;
     boolean aDown = false;
     boolean bDown = false;
-    boolean xDown = false;
-    boolean yDown = false;
     boolean rbDown = false;
+    boolean xDown = false;
+    boolean lbDown = false;
+    boolean dpadUp = false;
+    boolean dpadDown = false;
+
 
     @Override
     public void init() {
@@ -50,8 +57,8 @@ public class driveBaseTesting extends TunableOpMode {
             leftPower = robot.getWheelPower(gamepad1.left_stick_y) - robot.getWheelPowerLinear(gamepad1.right_stick_x);
             rightPower = robot.getWheelPower(gamepad1.left_stick_y) + robot.getWheelPowerLinear(gamepad1.right_stick_x);
 
-            robot.leftDrive.setPower(leftPower);
-            robot.rightDrive.setPower(rightPower);
+            robot.leftDrive.setPower(leftPower * direction + power);
+            robot.rightDrive.setPower(rightPower * direction + power);
 
 
             yDown = gamepad1.y;
@@ -59,6 +66,9 @@ public class driveBaseTesting extends TunableOpMode {
             xDown = gamepad1.x;
             bDown = gamepad1.b;
             rbDown = gamepad1.right_bumper;
+            lbDown = gamepad1.left_bumper;
+            dpadUp = gamepad1.dpad_up;
+            dpadDown = gamepad1.dpad_down;
 
             if(yDown){
                 robot.ADM.setTargetPosition((int)(robot.LEAD_SCREW_TURNS * robot.COUNTS_PER_MOTOR_REV));
@@ -92,6 +102,22 @@ public class driveBaseTesting extends TunableOpMode {
             if(rbDown){
                 robot.traverse.setPosition(robot.midTraverse);
                 rbDown = false;
+            }
+
+            if(lbDown) {
+                direction *= -1;
+            }
+
+            if(dpadDown) {
+                if(power != 0) {
+                    power -= 25;
+                }
+            }
+
+            if(dpadUp) {
+                if(power != 25) {
+                    power += 25;
+                }
             }
 
 
